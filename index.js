991766d674f9dd87423adfb824aa904c76f7e121
Bharat Kalra <bharat.kalra244@gmail.com>
2022-03-11 21:33:06 +0530

    port change

--- index.js
+++ index.js
@@ -158,6 +158,6 @@ app.get('/logoff', (req, res) => {
 	res.redirect('/')
 })
 
-app.listen(port, () => {
-	console.log(`Server running at http://localhost:${port}`)
-})
+app.listen(process.env.PORT || 3000, function(){
+	console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
